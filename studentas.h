08d4038e7c53187bb4d39c107de79437b6975c86
JFour404 <116594512+JFour404@users.noreply.github.com>
2023-05-04 19:30:31 +0300

    Update studentas.h

--- studentas.h
+++ studentas.h
@@ -8,11 +8,13 @@ private:
     int m_Egz;
     double m_GalBalas;
 public:
+    //kostruktoriai
     studentas () : m_Vardas(""), m_Pavarde("") {}
     studentas (string vardas, string pavarde, vector<int> paz, int egz) : m_Vardas(vardas), m_Pavarde(pavarde), m_Paz(paz), m_Egz(egz) {}
     studentas (istream& is, int baloSkc, int pazPildymas, int kiekis);
     studentas (stringstream& ss);
 
+    //set'eriai
     void Vardas (string vardas) { m_Vardas = vardas; }
     void Pavarde (string pavarde) { m_Pavarde = pavarde; }
     void Paz (vector <int> paz) { m_Paz = paz; }
@@ -20,6 +22,7 @@ public:
     void Egz (int egz) { m_Egz = egz; }
     void GalBalas (int baloSkc) { baloSkc == 0 ? m_GalBalas = mediana() : m_GalBalas = vidurkis();}
     
+    //get'eriai
     inline string Vardas () const { return m_Vardas; }
     inline string Pavarde () const { return m_Pavarde; }
     inline vector<int> Paz () const { return m_Paz; }
@@ -29,6 +32,8 @@ public:
     double vidurkis();
     double mediana();
     void randomGradeGen(int kiekis);
+
+    ~studentas () {}
 };
 
 bool palygintiPavardes(const studentas& a, const studentas& b);
